0ef5ab778cc99ecd9564876cdb041e271c8500f2
Shubham Gupta <sg350348@@gmail.com>
2020-07-10 07:56:28 +0530

    post is created and shown on home page

--- Codeial/controllers/home_controller.js
+++ Codeial/controllers/home_controller.js
@@ -1,6 +1,17 @@
+const Post = require('../models/post');
 module.exports.home=function(req,res){
     // return res.end("<h1>Express is up for codeial</h1>");
-    return res.render('home',{
-        title:"Home"
-    });
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title:"Codeial | Home",
+    //         posts:posts
+    //     });  
+    // });
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title:"Codeial | Home",
+            posts:posts
+        });
+    })
 }
--- Codeial/routes/index.js
+++ Codeial/routes/index.js
@@ -4,6 +4,7 @@ console.log("Router loaded");
 const homeController = require('../controllers/home_controller');
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
 
 
 module.exports=router;
