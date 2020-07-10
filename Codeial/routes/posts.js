41edcbca9e3b991890ad2de08dedd69206895f4d
Shubham Gupta <sg350348@@gmail.com>
2020-07-10 10:00:20 +0530

    deletion of post and comment is added

--- Codeial/routes/posts.js
+++ Codeial/routes/posts.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const passport=require('passport')
 const postController = require('../controllers/posts_controller');
 router.post('/create',passport.checkAuthentication,postController.create);
+router.get('/destroy/:id',passport.checkAuthentication,postController.destroy);
 // router.post('/create',postController.create);
 module.exports = router;
