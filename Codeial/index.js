5b3dd8901d71b06191e7b9c74087a2d8dab663ab
Shubham Gupta <sg350348@@gmail.com>
2020-09-05 07:16:52 +0530

    styling is added to sign-up and sign-in page

--- Codeial/index.js
+++ Codeial/index.js
@@ -9,11 +9,18 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const passportJWT = require('./config/passport-jwt-strategy');
-const passportGoogle = require('./config/passport-google-oauth2-strategy'); 
+const passportGoogle = require('./config/passport-google-oauth2-strategy');
+
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
+
+// setup the chat server to be used with socket.io
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('chat server is listening on port 5000');
 
 
 app.use(sassMiddleware({
